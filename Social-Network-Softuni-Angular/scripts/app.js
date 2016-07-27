1e2fc4141a5b9f91a14862be27ddc15d02737503
skloser <krum.bagashev@gmail.com>
2016-07-27 16:20:11 +0300

    Created ads

---
+++ Social-Network-Softuni-Angular/scripts/app.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="c:\users\k.bagashev\documents\visual studio 2015\Projects\Social-Network-Softuni-Angular\Social-Network-Softuni-Angular\libs/angular.js" />
+/// <reference path="c:\users\k.bagashev\documents\visual studio 2015\Projects\Social-Network-Softuni-Angular\Social-Network-Softuni-Angular\libs/angular-route.js" />
+
+"use strict";
+
+var app = angular.module("SocialNetwork", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "templates/home/home.html",
+        controller: "HomeController"
+    })
+    .otherwise({ redirectTo: "/" });
+});
---
+++ Social-Network-Softuni-Angular/scripts/home/home.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="../app.js" />
+/// <reference path="c:\users\k.bagashev\documents\visual studio 2015\Projects\Social-Network-Softuni-Angular\Social-Network-Softuni-Angular\libs/angular.js" />
+/// <reference path="c:\users\k.bagashev\documents\visual studio 2015\Projects\Social-Network-Softuni-Angular\Social-Network-Softuni-Angular\libs/angular-route.js" />
+
+"use strict";
+
+app.controller("HomeController", ["$scope", "$http", function ($scope, $http) {
+    $scope.tests = ["test1", "test2", "test3"];
+}]);
